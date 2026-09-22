d92fb8cda2fa8fe6f1523634ff620ad70905b5bd
yamei <1842850321@qq.com>
2026-09-22 17:04:14 +0800

    Document modular benchmark package and preserve legacy entrypoints

--- catalogue.js
+++ catalogue.js
@@ -4283,4 +4283,4 @@ window.HYPERBENCH_CASES = [
     "suite": "complex"
   }
 ];
-window.HYPERBENCH_REVISION = "CODE / bb469ea5a \u00b7 2026-09-22";
+window.HYPERBENCH_REVISION = "CODE / cd7552739 \u00b7 2026-09-22";
